ae4488c03ceff2dbb86eeca69f30eb2ea703a21b
Franco <frfranco@uci.edu>
2017-06-27 11:38:20 -0700

    checkID() with users table

--- access-denied.php
+++ access-denied.php
@@ -3,6 +3,7 @@
 	<br>
 	Please contact the UCI MIND Tissue Repository in order to gain access.
 	<?php
+		// redirect after 5 seconds
 		header( "refresh:5;url=https://www.mind.uci.edu/adrc/neuropathology-core/");
 	?>
 <h1>
