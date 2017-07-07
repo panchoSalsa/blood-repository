cafff1b73f35c912b7d2d1ef64d5a99be4819952
Franco <frfranco@uci.edu>
2017-07-07 11:03:49 -0700

    making progress with blood-sample form

--- add-blood-sample.php
+++ add-blood-sample.php
@@ -14,20 +14,28 @@
 
         <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
 
+        <link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+
         <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'  type='text/css'>
 <!--         <link rel="stylesheet" type="text/css" href="css/style.css"> -->
 
 
 		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script> 
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>-->
+
+    <!-- down-graded to jQuery 2.1 so that I can use bootstrap's date picker  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 
 		<!-- Latest compiled JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-        <script src="js/ui.core.js" type="text/javascript"></script>
-        <script src="js/ui.tabs.js" type="text/javascript"></script>
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script> -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+
 
 
 <style>
@@ -78,15 +86,65 @@ fieldset.scheduler-border {
              <fieldset class="scheduler-border">
                 <legend class="scheduler-border">Blood Sample Form</legend>
 
-                <div class="form-group">
+<!--                 <div class="form-group"> -->
 <!--                    <label for="study">Study</label> 
                    <div>
                       <input id="study" class="form-control" name="study" type="text">
                     </div> -->
+
+                <div class="row">
+
+                  <div class="col-sm-4">
                    <label for="patient_id">Patient ID</label>  
                    <div>
                       <input id="patient_id" class="form-control" name="patient_id" type="text">
                    </div>
+                  </div>
+
+                  <div class="col-sm-4">
+                   <label for="visit">Visit</label> 
+                   <div>
+                      <input id="visit" class="form-control" name="visit" type="text">
+                    </div>
+                  </div>
+
+                  <div class="col-sm-4">
+
+                          <label for="visit_date">Visit Date</label>
+                          <div class="form-group">
+                                <div class="input-group date" id="datetimepicker_visit_date">
+                                    <input id="visit_date" class="form-control" name="visit_date" type="text"/>
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-calendar"></span>
+                                    </span>
+                                </div>
+                          </div>
+
+<!-- 
+                    <label for="visit_date">Visit Date</label>  
+                    <div>
+                      <input id="visit_date" class="form-control" name="visit_date" type="text" placeholder="MM/DD/YYYY" required pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}">
+                   </div> -->
+                  </div>
+                </div>
+
+
+<!-- 
+                <div class="container">
+                    <div class="row">
+                        <div class='col-sm-6'>
+                            <div class="form-group">
+                                <div class='input-group date' id='datetimepicker1'>
+                                    <input type='text' class="form-control" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-calendar"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
 <!--                    <label for="synd">Syndrome</label>  
                    <div>
                       <input id="synd" class="form-control" name="synd" type="text">
@@ -99,14 +157,11 @@ fieldset.scheduler-border {
                    <div>
                       <input id="dx" class="form-control" name="dx" type="text">
                    </div> -->
-                   <label for="visit">Visit</label> 
-                   <div>
-                      <input id="visit" class="form-control" name="visit" type="text">
-                    </div>
-                  <label for="visit_date">Visit Date</label>  
+
+<!--                   <label for="visit_date">Visit Date</label>  
                     <div>
                       <input id="visit_date" class="form-control" name="visit_date" type="text" placeholder="MM/DD/YYYY" required pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}">
-                   </div>
+                   </div> -->
 <!--                    <label for="age">Age</label> 
                    <div>
                       <input id="age" class="form-control" name="age" type="text">
@@ -135,7 +190,45 @@ fieldset.scheduler-border {
                       <input id="staff" class="form-control" name="staff" type="text">
                    </div> -->
 
+
+                <div class="row">
+                  <div class="col-sm-4">
                    <label for="freezer_id">Freezer ID</label> 
+                   <div>
+                      <input id="freezer_id" class="form-control" name="freezer_id" type="text">
+                    </div>
+                  </div>
+
+                  <div class="col-sm-4">
+                   <label for="freezer_rack">Freezer Rack</label> 
+                   <div>
+                      <input id="freezer_rack" class="form-control" name="freezer_rack" type="text">
+                    </div>
+                  </div>
+
+                  <div class="col-sm-4">
+
+                          <label for="frozen_date">Frozen Date</label> 
+                          <div class="form-group">
+                                <div class="input-group date" id="datetimepicker_frozen_date">
+                                    <input id="frozen_date" class="form-control" name="frozen_date" type="text"/>
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-calendar"></span>
+                                    </span>
+                                </div>
+                          </div>
+
+
+
+
+<!--                    <label for="frozen_date">Frozen Date</label> 
+                   <div>
+                      <input id="frozen_date" class="form-control" name="frozen_date" type="text" placeholder="MM/DD/YYYY" required pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}">
+                    </div>
+                  </div> -->
+                </div>
+
+<!--                    <label for="freezer_id">Freezer ID</label> 
                    <div>
                       <input id="freezer_id" class="form-control" name="freezer_id" type="text">
                     </div>
@@ -146,7 +239,7 @@ fieldset.scheduler-border {
                    <label for="frozen_date">Frozen Date</label> 
                    <div>
                       <input id="frozen_date" class="form-control" name="frozen_date" type="text" placeholder="MM/DD/YYYY" required pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}">
-                    </div>
+                    </div> -->
 <!--                    <label for="frozen_time">Frozen Time</label>  
                    <div>
                       <input id="frozen_time" class="form-control" name="frozen_time" type="text">
@@ -173,58 +266,75 @@ fieldset.scheduler-border {
                       <textarea id="comments" class="form-control" name="comments" rows="4"></textarea>
                    </div> -->
                 </div>
+
+
+<!-- source=https://eonasdan.github.io/bootstrap-datetimepicker/#custom-icons -->
+                    <div class="row">
+                        <div class='col-sm-6'>
+                            <div class="form-group">
+                                <div class='input-group date' id='datetimepicker3'>
+                                    <input type='text' class="form-control" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 <div class="row">
                   <div class="col-sm-6">
-
-                  <legend>Sample 1</legend>
-                  <div class="form-group">
-                     <label for="sample_1_box_id">Box ID</label>  
-                     <div>
-                        <input id="sample_1_box_id" class="form-control" name="sample_1_box_id" type="text">
-                     </div>
-                     <label for="sample_1_box_row">Box Row</label>  
-                     <div>
-                        <input id="sample_1_box_rowv" class="form-control" name="sample_1_box_row" type="text">
-                     </div>
-                     <label for="sample_1_box_column">Box Column</label>  
-                     <div>
-                        <input id="sample_1_box_column" class="form-control" name="sample_1_box_column" type="text">
-                     </div>
-                  </div>
-
-
+                    <legend>Sample 1</legend>
+                    <div class="form-group">
+                       <label for="sample_1_box_id">Box ID</label>  
+                       <div>
+                          <input id="sample_1_box_id" class="form-control" name="sample_1_box_id" type="text">
+                       </div>
+                       <label for="sample_1_box_row">Box Row</label>  
+                       <div>
+                          <input id="sample_1_box_rowv" class="form-control" name="sample_1_box_row" type="text" placeholder="A-H" required pattern="[A-H]">
+                       </div>
+                       <label for="sample_1_box_column">Box Column</label>  
+                       <div>
+                          <input id="sample_1_box_column" class="form-control" name="sample_1_box_column" type="text" placeholder="1-16" required pattern="[1-9]{2}">
+                       </div>
+                    </div>
                   </div>
 
 
                   <div class="col-sm-6">
-
-                  <legend>Sample 2</legend>
-                  <div class="form-group">
-                     <label for="sample_2_box_id">Box ID</label>  
-                     <div>
-                        <input id="sample_2_box_id" class="form-control" name="sample_2_box_id" type="text">
-                     </div>
-                     <label for="sample_2_box_row">Box Row</label>  
-                     <div>
-                        <input id="sample_2_box_rowv" class="form-control" name="sample_2_box_row" type="text">
-                     </div>
-                     <label for="sample_2_box_column">Box Column</label>  
-                     <div>
-                        <input id="sample_2_box_column" class="form-control" name="sample_2_box_column" type="text">
-                     </div>
+                    <legend>Sample 2</legend>
+                    <div class="form-group">
+                       <label for="sample_2_box_id">Box ID</label>  
+                       <div>
+                          <input id="sample_2_box_id" class="form-control" name="sample_2_box_id" type="text">
+                       </div>
+                       <label for="sample_2_box_row">Box Row</label>  
+                       <div>
+                          <input id="sample_2_box_rowv" class="form-control" name="sample_2_box_row" type="text" placeholder="A-H" required pattern="[A-H]">
+                       </div>
+                       <label for="sample_2_box_column">Box Column</label>  
+                       <div>
+                          <input id="sample_2_box_column" class="form-control" name="sample_2_box_column" type="text" placeholder="1-16" required pattern="[1-9]{2}">
+                       </div>
+                    </div>
                   </div>
-
-
-                  </div>
-
-
                 </div>
 
                 <!-- Button (Double) -->
+<!--                 <div class="row">
                 <div class="form-group">
                    <div class="col-md-8">
                       <button type="submit" class="btn btn-success">Submit</button>
                    </div>
+                </div> -->
+
+                <div class="row">
+                  <div class="col-md-8">
+                   <div class="form-group">
+                      <button type="submit" class="btn btn-success">Submit</button>
+                   </div>
+                  </div>
                 </div>
 
              </fieldset>
@@ -238,6 +348,27 @@ fieldset.scheduler-border {
                 window.location= "search.php?id=" + patient_id;
                 return false;
             }
+        </script>
+
+        <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker_visit_date').datetimepicker({
+                  format: 'MM/DD/YYYY'
+                });
+
+                $('#datetimepicker_frozen_date').datetimepicker({
+                  format: 'MM/DD/YYYY'
+                });
+            });
+        </script>
+
+
+        <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker3').datetimepicker({
+                    format: 'LT'
+                });
+            });
         </script>
 
         <script src="js/header.js" type="text/javascript"></script>
